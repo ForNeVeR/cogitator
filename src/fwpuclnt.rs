acc2722d043138f9fe2629eff6263969a443df33
Friedrich von Never <friedrich@fornever.me>
2016-12-25 13:33:17 +0700

    fwpuclnt: add FwpmEngineOpen0 function

---
+++ src/fwpuclnt.rs
@@ -0,0 +1,29 @@
+#![allow(dead_code, non_camel_case_types, non_snake_case)]
+
+use winapi::*;
+
+#[repr(C)]
+pub struct FWPM_DISPLAY_DATA0 {
+    pub name: *mut wchar_t,
+    pub description: *mut wchar_t,
+}
+
+#[repr(C)]
+pub struct FWPM_SESSION0 {
+    pub sessionKey: GUID,
+    pub displayData: FWPM_DISPLAY_DATA0,
+    pub flags: UINT32,
+    pub txnWaitTimeoutInMSec: UINT32,
+    pub processId: DWORD,
+    pub sid: PSID,
+    pub username: *const wchar_t,
+    pub kernelMode: BOOL,
+}
+
+extern "system" {
+    pub fn FwpmEngineOpen0(
+        serverName: *const wchar_t, authnService: UINT32,
+        authIdentity: *mut SEC_WINNT_AUTH_IDENTITY_W, session: *const FWPM_SESSION0,
+        engineHandle: *mut HANDLE,
+   ) -> DWORD;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate winapi;
+
+mod fwpuclnt;
+
 fn main() {
     println!("Hello, world!");
 }
