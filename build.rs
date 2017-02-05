f9fe3c19719ae469c5a86637442372d0621825e8
Friedrich von Never <friedrich@fornever.me>
2017-02-05 18:26:58 +0700

    #1: copy DLL and the driver to output

--- build.rs
+++ build.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
     println!(r"cargo:rustc-link-search=windivert\WinDivert-1.1.8-MSVC\amd64");
+    let target = env::var("OUT_DIR").unwrap();
+    fs::copy(
+        r"windivert\WinDivert-1.1.8-MSVC\amd64\WinDivert.dll",
+        Path::new(&target).join(r"..\..\..\WinDivert.dll"));
+    fs::copy(
+        r"windivert\WinDivert-1.1.8-MSVC\amd64\WinDivert64.sys",
+        Path::new(&target).join(r"..\..\..\WinDivert64.sys"));
 }
